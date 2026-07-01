7476175f4f8b1293b1647874e0336f5236794f7b
Abhay8777 <mishraabhay8777@gmail.com>
2026-07-01 22:45:39 +0530

    Time: 67 ms (94.67%), Space: 106.5 MB (83.11%) - LeetHub

--- 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.cpp
+++ 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.cpp
@@ -1,56 +1,53 @@
 class Solution {
 public:
-    void updateFreq(int operation, int val, vector<int> &freq) {
+    void updateFreq(int op, int val, vector<int>& freqBits) {
         int i = 0;
         while (val > 0) {
-            if (val & 1) {
-                freq[i] += operation;
-            }
+            if (val & 1)
+                freqBits[i] += op;
             val >>= 1;
             i++;
         }
     }
 
-    int buildOR(vector<int> &freq) {
-        int Or = 0;
-        for (int b = 0; b < 32; b++) {
-            if (freq[b] > 0) {
-                Or |= (1 << b);
-            }
-        }
-        return Or;
-    }
-
     int minimumDifference(vector<int>& nums, int k) {
+        vector<int> freqBits(32, 0);
+
         int n = nums.size();
-
-        vector<int> freq(32, 0);
-
         int i = 0, j = 0;
         int result = INT_MAX;
+        int windowOr = 0;
 
         while (j < n) {
 
-            updateFreq(1, nums[j], freq);
+            if (i > j)
+                windowOr = 0;
 
-            int Or = buildOR(freq);
+            windowOr |= nums[j];
+            updateFreq(1, nums[j], freqBits);
 
-            result = min(result, abs(k - Or));
+            result = min(result, abs(k - windowOr));
 
             if (result == 0)
                 return 0;
 
-            while (i <= j && Or > k) {
+            while (i <= j && windowOr > k) {
 
-                updateFreq(-1, nums[i], freq);
+                updateFreq(-1, nums[i], freqBits);
                 i++;
 
-                if (i > j)
-                    break;          // Don't consider empty window
+                if (i > j) {
+                    windowOr = 0;
+                    break;
+                }
 
-                Or = buildOR(freq);
+                windowOr = 0;
+                for (int b = 0; b < 32; b++) {
+                    if (freqBits[b] > 0)
+                        windowOr |= (1 << b);
+                }
 
-                result = min(result, abs(k - Or));
+                result = min(result, abs(k - windowOr));
 
                 if (result == 0)
                     return 0;
