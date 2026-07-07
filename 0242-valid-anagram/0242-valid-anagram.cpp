80b9e470927e93484662d2207467036ce460ca5d
Abhay8777 <mishraabhay8777@gmail.com>
2026-07-08 01:07:18 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.73%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> freq1, freq2;
+        for(char c : s) freq1[c]++;
+    
+        for(char c : t) freq2[c]++;
+        if(freq1!=freq2) return false;
+        return true;
+    }
+};
