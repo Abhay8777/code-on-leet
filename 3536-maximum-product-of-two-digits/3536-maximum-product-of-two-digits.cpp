9edfca8d3bb9b7b46bb12bac0236f732e9c26abc
Abhay8777 <mishraabhay8777@gmail.com>
2026-07-25 21:50:52 +0530

    Time: 3 ms (15.04%), Space: 9 MB (42.3%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -6,16 +6,9 @@ public:
             digits.push_back(n % 10);
             n /= 10;
         }
-        
-        int max1 = 0, max2 = 0;
-        for(int d : digits){
-            if(d > max1){
-                max2 = max1;
-                max1 = d;
-            } else if(d > max2){
-                max2 = d;
-            }
-        }
-        return max1 * max2;
+        int m = digits.size()-1;
+        sort(digits.begin(),digits.end());
+        int ans = digits[m]*digits[m-1];
+        return ans;
     }
 };
