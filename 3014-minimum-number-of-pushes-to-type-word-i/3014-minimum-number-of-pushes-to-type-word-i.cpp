3f4088f1c4a5e03d6fda152cf2a9dfeaa960ff94
Abhay8777 <mishraabhay8777@gmail.com>
2026-07-30 16:52:58 +0530

    Time: 0 ms (100%), Space: 8.5 MB (27.63%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+         vector<int>mp(26,0);
+        int result = 0;
+        for(char &ch: word){
+            mp[ch-'a']++;
+        }
+        sort(begin(mp),end(mp),greater<int>()); // we first store the greater number of frequency
+        for(int i = 0;i<26;i++){
+            int pass  = i/8 +1;
+            result+= pass*mp[i];
+        }
+        return result;
+    }
+};
