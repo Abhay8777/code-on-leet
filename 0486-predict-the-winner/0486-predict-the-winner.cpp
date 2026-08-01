0a51925e4f10c622da9d33c3771b6e41360ccf0a
Abhay8777 <mishraabhay8777@gmail.com>
2026-08-02 00:04:16 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.44%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        // Base case: single element
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        
+        // Fill DP table for subarrays of length >= 2
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                int pickLeft = nums[i] - dp[i+1][j];
+                int pickRight = nums[j] - dp[i][j-1];
+                dp[i][j] = max(pickLeft, pickRight);
+            }
+        }
+        
+        return dp[0][n-1] >= 0;
+    }
+};
