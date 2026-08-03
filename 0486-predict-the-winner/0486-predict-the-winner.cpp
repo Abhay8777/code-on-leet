b77046706f50a585536e214b362682c49e6a8095
Abhay8777 <mishraabhay8777@gmail.com>
2026-08-03 17:00:29 +0530

    Time: 0 ms (100%), Space: 9.6 MB (84.07%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,27 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
+    int t[23][23];
+    int solve(int i, int j, vector<int>& nums){
+        
+        if(i>j){
+            return 0;
+        }
+        if(i==j) return nums[i];
+        if(t[i][j]!=-1) return t[i][j];
+        int take_i = nums[i] + min(solve(i+2,j,nums), solve(i+1, j-1,nums));
+        int take_j = nums[j] + min(solve(i, j-2, nums), solve(i+1, j-1,nums));
+        return t[i][j]=max(take_i,take_j);
+    }
     bool predictTheWinner(vector<int>& nums) {
+        memset(t, -1, sizeof(t));
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
-        // Base case: single element
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = nums[i];
-        }
-        
-        // Fill DP table for subarrays of length >= 2
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                int pickLeft = nums[i] - dp[i+1][j];
-                int pickRight = nums[j] - dp[i][j-1];
-                dp[i][j] = max(pickLeft, pickRight);
-            }
-        }
-        
-        return dp[0][n-1] >= 0;
+        int sum = accumulate(begin(nums),end(nums),0);
+        int p1 = solve(0,n-1, nums);
+        int p2  = sum- p1;
+        return p1>=p2;
     }
 };
