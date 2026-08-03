2b436b5b2add0e8e50e329466d896db978fae602
Abhay8777 <mishraabhay8777@gmail.com>
2026-08-03 17:11:11 +0530

    Time: 0 ms (100%), Space: 9.4 MB (95.73%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int t[23][23];
-    int solve(int i, int j, vector<int>& nums){
+    int solve(vector<int>& nums,int i, int j){
         
         if(i>j){
             return 0;
         }
         if(i==j) return nums[i];
         if(t[i][j]!=-1) return t[i][j];
-        int take_i = nums[i] + min(solve(i+2,j,nums), solve(i+1, j-1,nums));
-        int take_j = nums[j] + min(solve(i, j-2, nums), solve(i+1, j-1,nums));
+        int take_i = nums[i] - solve(nums,i+1, j);
+        int take_j = nums[j] - solve(nums, i, j-1);
         return t[i][j]=max(take_i,take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         memset(t, -1, sizeof(t));
         int n = nums.size();
-        int sum = accumulate(begin(nums),end(nums),0);
-        int p1 = solve(0,n-1, nums);
-        int p2  = sum- p1;
-        return p1>=p2;
+        return solve(nums,0,n-1)>=0;
+        
     }
 };
