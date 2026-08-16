e4a296d3c542abe906d4acacbc3129a7b0910306
Abhay <mishraabhay8777@gmail.com>
2026-08-16 23:42:14 +0530

    Time: 4 ms (62.44%), Space: 131.3 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0, 0, 0};
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        // If one of remainder 1 or 2 is absent,
+        // Alice must start with the other one.
+        if (min(cnt[1], cnt[2]) == 0) {
+            return max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 == 1;
+        }
+
+        // Both remainder 1 and 2 exist.
+        return abs(cnt[1] - cnt[2]) > 2 || cnt[0] % 2 == 0;
+    }
+};
